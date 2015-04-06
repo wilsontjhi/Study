94943c444d6afac6b346f23d6c8062b7e7cc3fb7
wilsontjhi <wilsontjhi@gmail.com>
2015-04-06 15:06:49 +0800

    Validation and styling for Index page

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -26,8 +26,15 @@ namespace PartyInvites.Controllers
         [HttpPost]
         public ViewResult RsvpForm(GuestResponse guestResponse)
         {
-            // TODO: Email response to the party organizer
-            return View("Thanks", guestResponse);
+            if (ModelState.IsValid)
+            {
+                // TODO: Email response to the party organizer
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
